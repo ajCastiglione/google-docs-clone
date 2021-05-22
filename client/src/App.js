71a999280e545ce94e655098be057bbc7c06f716
Antonio Castiglione <antonio@minervawebdevelopment.com>
2021-05-22 09:05:10 -0400

    updated syntax

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,14 @@ import {
     Route,
     Redirect,
 } from "react-router-dom";
-import { v4 as uuidV4 } from "uuid";
+import { v4 } from "uuid";
 
 function App() {
     return (
         <Router>
             <Switch>
                 <Route path="/" exact>
-                    <Redirect to={`/documents/${uuidV4()}`} />
+                    <Redirect to={`/documents/${v4()}`} />
                 </Route>
                 <Route path="/documents/:id">
                     <TextEditor />
