f8e4b0b96c7831d6d144c2ac1faaf2e5536efda1
Antonio Castiglione <antonio@minervawebdevelopment.com>
2023-06-10 18:31:11 -0400

    updated url to point to new backend

--- client/src/config/url.js
+++ client/src/config/url.js
@@ -1,5 +1,5 @@
 const local = "http://localhost:3001";
-const prod = "https://google-docs-clone-mwd.herokuapp.com/";
+const prod = "https://google-docs-clone-backend-1xz0.onrender.com/";
 
 export function getUrl() {
     if (window.location.host.includes("localhost")) {
